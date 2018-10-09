c3d0e8e6905320ef9c548685a4560a03f0a20c15
LuigiKerb <43919313+LuigiKerb@users.noreply.github.com>
2018-10-08 21:31:14 -0300

    Update Heap_Sort.c

--- Heap_Sort.c
+++ Heap_Sort.c
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <string.h>
 
+//HeapFy que executara a heap maxima do vetor
 void heapfy(int *elements, int n, int i){
     int maior = i,temp;
     int esquerda = 2*i + 1;
@@ -29,18 +30,14 @@ void heapfy(int *elements, int n, int i){
 
         heapfy(elements,n,maior);
     }
-
-   
 }
-void heapsort(int *elements,int n)
-{
+//algoritmo heapsort de ordenação em ordem crescente
+void heapsort(int *elements,int n){
     int i,temp;
-    for(i = n/2-1;i >= 0;i--)
-    {
+    for(i = n/2-1;i >= 0;i--){
         heapfy(elements,n,i);
     }
-    for(i = n-1;i>=0;i--)
-    {
+    for(i = n-1;i>=0;i--){
         temp = elements[0];
         elements[0] = elements[i];
         elements[i] = temp; 
@@ -49,100 +46,83 @@ void heapsort(int *elements,int n)
     }
 }
 
+//Função com intuito de preencher o vetor com o tamanho necessário
+//7 = Aleatorio; 8 = Crescente; 9 = Decrescente
 void vetor(int x,int y, int v[]){
 	FILE *file;
 	int a;
-	if(x==10 && y==7)
-	{ 
-		
+	//Abre o arquivo aleatorio com 10 elementos
+	if(x==10 && y==7){		
 		file = fopen("Aleatorio10.txt","r");
-		
 	}
-	else if(x==1000 && y==7)
-	{
+	//Abre o arquivo aleatorio com 1000 elementos
+	else if(x==1000 && y==7){
 		file = fopen("Aleatorio1000.txt","r");
-
 	}
-	else if(x==10000 && y==7)
-	{
+	//Abre o arquivo aleatorio com 10000 elementos
+	else if(x==10000 && y==7){
 		file = fopen("Aleatorio10000.txt","r");
-
 	}
-	else if(x==100000 && y==7)
-	{
+	//Abre o arquivo aleatorio com 100000 elementos
+	else if(x==100000 && y==7){
 		file = fopen("Aleatorio100000.txt","r");
-
 	}
-	else if(x==1000000 && y==7)
-	{
+	//Abre o arquivo aleatorio com 1000000 elementos
+	else if(x==1000000 && y==7){
 		file = fopen("Aleatorio1000000.txt","r");
-
 	}
-	else if(x==10 && y==8)
-	{
+	//Abre o arquivo crescente com 10 elementos
+	else if(x==10 && y==8){
 		file = fopen("Crescente10.txt","r");
-		
 	}
-	else if(x==1000 && y==8)
-	{
+	//Abre o arquivo crescente com 1000 elementos
+	else if(x==1000 && y==8){
 		file = fopen("Crescente1000.txt","r");
-
 	}
-	else if(x==10000 && y==8)
-	{
+	//Abre o arquivo crescente com 10000 elementos
+	else if(x==10000 && y==8){
 		file = fopen("Crescente10000.txt","r");
-
 	}
-	else if(x==100000 && y==8)
-	{
+	//Abre o arquivo crescente com 100000 elementos
+	else if(x==100000 && y==8){
 		file = fopen("Crescente100000.txt","r");
-
 	}
-	else if(x==1000000 && y==8)
-	{
-		file = fopen("Crescente1000000.txt","r");
-
+	//Abre o arquivo crescente com 1000000 elementos
+	else if(x==1000000 && y==8){
+		file = fopen("Crescente1000000.txt","r")
 	}
-	else if(x==10 && y==9)
-	{
+	//Abre o arquivo decrescente com 10 elementos
+	else if(x==10 && y==9){
 		file = fopen("Decrescente10.txt","r");
-		
 	}
-	else if(x==1000 && y==9)
-	{
+	//Abre o arquivo decrescente com 1000 elementos
+	else if(x==1000 && y==9){
 		file = fopen("Decrescente1000.txt","r");
-
 	}
-	else if(x==10000 && y==9)
-	{
+	//Abre o arquivo decrescente com 10000 elementos
+	else if(x==10000 && y==9){
 		file = fopen("Decrescente10000.txt","r");
-
 	}
-	else if(x==100000 && y==9)
-	{
+	//Abre o arquivo decrescente com 100000 elementos
+	else if(x==100000 && y==9){
 		file = fopen("Decrescente100000.txt","r");
-
 	}
-	else if(x==1000000 && y==9)
-	{
+	//Abre o arquivo decrescente com 1000000 elementos
+	else if(x==1000000 && y==9){
 		file = fopen("Decrescente1000000.txt","r");
-
 	}
 	
 	int n = 0;
-    if(file==NULL)
-	{
-        printf("Erro ao abrir o arquivo.\n");
-        
-    } 
-   
-    for (n = 0; n < x; n++){
-			fscanf(file,"%d\n", &a);
-			v[n]=a;
-		}
-	
-    fclose(file);
-
+	//Caso nao tenha espaço para armazenar
+	if(file==NULL){
+        	printf("Erro ao abrir o arquivo.\n");
+	} 
+	//Lê o arquivo e armazena no vetor
+   	for (n = 0; n < x; n++){
+		fscanf(file,"%d\n", &a);
+		v[n]=a;
+	}
+	fclose(file);
 }
 
 int main(){
@@ -153,6 +133,7 @@ int main(){
 	heap = fopen("Heap_Sort_Final.txt", "w");
 	int i = 10, j = 0, k = 7;
 	//int j = 0;
+	//vai executar todas as entradas e armazenar o tempo em segundos num arquivo txt
 	for(k = 7; k < 10; k++){
 		printf("Com %d: ", k);
 		for(i = 10; i <= 1000000; i=i*10){
